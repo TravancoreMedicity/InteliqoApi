1b669a1d4ba81fc2e078a9daad7f4f26e2c35e41
unknown <Administrator@ITSYS13.medicity.co.in>
2022-07-16 12:44:54 +0530

    institution report copy deleted

--- api/Reports/Reports.service.js
+++ api/Reports/Reports.service.js
@@ -662,35 +662,6 @@ module.exports = {
             }
         )
     },
-    InstitutionReport: (data, callBack) => {
-        pool.query(
-            `SELECT 
-            hrm_emp_master.em_no,
-            hrm_emp_master.em_name,
-            hrm_department.dept_name,
-            hrm_dept_section.sect_name,
-             hrm_branch.branch_name,
-            institution_type.inst_emp_type,
-            designation.desg_name,
-            hrm_emp_master.em_doj
-            FROM medi_hrm.institution_type
-            left join hrm_emp_master on hrm_emp_master.em_institution_type=institution_type.inst_slno
-            left join hrm_department on hrm_department.dept_id=hrm_emp_master.em_department
-            left join hrm_dept_section on hrm_dept_section.sect_id=hrm_emp_master.em_dept_section
-            left join hrm_branch on hrm_emp_master.em_branch = hrm_branch.branch_slno
-            left join designation on hrm_emp_master.em_designation = designation.desg_slno
-            where hrm_emp_master.em_status = 1 and institution_type.inst_slno IN(?)`,
-            [
-                data
-            ],
-            (error, results, feilds) => {
-                if (error) {
-                    return callBack(error);
-                }
-                return callBack(null, results);
-            }
-        )
-    },
     getDeptSectByID: (id, callback) => {
         pool.query(
             `SELECT sect_id,sect_name FROM hrm_dept_section WHERE dept_id IN (?)`,
