25356e833b390c60b155ea8762b55bf3fc46d0f5
unknown <Administrator@ITSYS13.medicity.co.in>
2022-11-04 11:27:45 +0530

    Job descritpion corrections

--- api/JobSummary/JobSummary.router.js
+++ api/JobSummary/JobSummary.router.js
@@ -7,35 +7,43 @@ const { createJobSummary, CheckInsertValue, createJobDuties, getjobId,
     updatejobsummarydetl, getjobcompetency, getjobDescView,
     updateDutiesEach, deleteduties, updateCompeteEach,
     deletecompetency, deletePerformance, updatePerforEach,
-    deleteQualifi, updateGeneric } = require('../JobSummary/JobSummary.controller');
+    deleteQualifi, updateGeneric, getKPIScore } = require('../JobSummary/JobSummary.controller');
 
+//job summery    
 router.post("/", checkToken, createJobSummary);
 router.post("/check", checkToken, CheckInsertValue);
-router.post("/jobduties", checkToken, createJobDuties);
-router.get("/", checkToken, getjobId);
-router.post("/jobspecification", checkToken, createJobSpecification);
-router.post("/jobqualification", checkToken, createJobQualification);
-router.post("/jobGeneric", checkToken, createJobGeneric);
-router.post("/getjobsummary", checkToken, getJobSummary);
-router.post("/getjobduties", checkToken, getJobDuties);
-router.post("/getjobspecific", checkToken, getJobSpecification);
-router.post("/getjobgeneric", checkToken, getJobGeneric);
-router.post("/getjobQual", checkToken, getJobQualification);
 router.get("/getjobSummary/:id", checkToken, getJobSummarydetl)
-router.post("/jobcompetency", checkToken, createJobCompetency)
+router.post("/getjobsummary", checkToken, getJobSummary);
 router.patch("/updatejobsummary", checkToken, updatejobsummarydetl)
-router.post('/get/jobcompetency', checkToken, getjobcompetency)
 
-router.get("/jobview", checkToken, getjobDescView)
-// to update duties and responsibilities each row 
+// duties & responsibilities 
+router.post("/jobduties", checkToken, createJobDuties);
 router.patch('/updatedutieseach', checkToken, updateDutiesEach)
 router.delete("/deletedata/select/:id", checkToken, deleteduties);
+router.get("/", checkToken, getjobId);
+router.post("/getjobduties", checkToken, getJobDuties);
+
+
+//job performance
+router.post("/jobspecification", checkToken, createJobSpecification);
+router.post("/getjobspecific", checkToken, getJobSpecification);
+router.delete('/deletePerf/:id', checkToken, deletePerformance)
+router.patch('/updateperf', checkToken, updatePerforEach)
+
+//job competency
+router.post("/jobcompetency", checkToken, createJobCompetency)
+router.post('/get/jobcompetency', checkToken, getjobcompetency)
 router.patch('/updatecompeteEach', checkToken, updateCompeteEach)
 router.delete('/deletecompet/:id', checkToken, deletecompetency)
-router.delete('/deletePerf/:id', checkToken, deletePerformance)
-//to update performance each row
-router.patch('/updateperf', checkToken, updatePerforEach)
+
+//job qualication
+router.post("/jobqualification", checkToken, createJobQualification);
+router.post("/jobGeneric", checkToken, createJobGeneric);
+router.post("/getjobgeneric", checkToken, getJobGeneric);
+router.post("/getjobQual", checkToken, getJobQualification);
+router.get("/jobview", checkToken, getjobDescView)
 router.delete('/deleteQualification/:id', checkToken, deleteQualifi)
 router.patch('/updategeneric', checkToken, updateGeneric)
+router.post('/getscore', checkToken, getKPIScore)
 
 module.exports = router;
