f00f635c7ab4d34ef5894af608ff8da9d46a27fd
unknown <Administrator@ITSYS13.medicity.co.in>
2024-01-03 09:46:09 +0530

    dutyplan

--- api/Departmentshiftmaster/Departmentshiftmaster.router.js
+++ api/Departmentshiftmaster/Departmentshiftmaster.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
-const { create, update, getSelect, getdepartmentshiftMasterByID, getDepartmentShiftbyshiftid, checkshiftforDept, getShiftTiming } = require('../Departmentshiftmaster/Departmentshiftmaster.controller');
+const { create, update, getSelect, getdepartmentshiftMasterByID,
+    getDepartmentShiftbyshiftid, checkshiftforDept, getShiftTiming,
+    checkshiftforSection, getSectionShiftbyshiftid } = require('../Departmentshiftmaster/Departmentshiftmaster.controller');
 
 router.post("/", checkToken, create);
 router.patch("/", checkToken, update);
@@ -9,4 +11,8 @@ router.get("/:id", checkToken, getdepartmentshiftMasterByID);
 router.post("/shift", checkToken, getDepartmentShiftbyshiftid);
 router.post("/checkshift", checkToken, checkshiftforDept);
 router.post("/getShiftTiming", checkToken, getShiftTiming);
+router.post("/checkSection", checkToken, checkshiftforSection)
+router.post("/SectionShift", checkToken, getSectionShiftbyshiftid)
+
+
 module.exports = router;
