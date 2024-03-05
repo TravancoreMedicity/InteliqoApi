cb04751b478a6e3e25e623d0f39cba9e0de94e43
unknown <Administrator@ITSYS13.medicity.co.in>
2024-03-05 16:07:21 +0530

    active employee report correction

--- api/ActiveEmpReport/ActiveEmpReport.service.js
+++ api/ActiveEmpReport/ActiveEmpReport.service.js
@@ -13,7 +13,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?))`,
             [
                 data
@@ -38,7 +38,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?) and hrm_department.dept_id IN (?))`,
             [
                 data.branch_slno,
@@ -64,7 +64,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?) AND hrm_department.dept_id IN (?) AND hrm_dept_section.sect_id IN (?))`,
             [
                 data.branch_slno,
@@ -91,7 +91,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?) and em_doj between ? and ?)`,
             [
                 data.branch_slno,
@@ -118,7 +118,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?) and hrm_department.dept_id IN (?) and em_doj between ? and ?)`,
             [
                 data.branch_slno,
@@ -146,7 +146,7 @@ module.exports = {
             inner join designation on designation.desg_slno=hrm_emp_master.em_designation
             inner join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
             inner join institution_type on institution_type.inst_slno=hrm_emp_master.em_institution_type
-            inner join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
+            left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
             where (hrm_emp_master.em_status=1 and hrm_emp_master.em_no!=1 and hrm_emp_master.em_no!=2) and (hrm_branch.branch_slno IN (?) and hrm_department.dept_id IN (?) and hrm_dept_section.sect_id IN (?)
             and em_doj between ? and ?)`,
             [
@@ -169,7 +169,7 @@ module.exports = {
         pool.query(
             `select hrm_emp_contract_log.em_id,old_emno as em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status,remark
             from hrm_emp_contract_log
             left join hrm_emp_master on hrm_emp_master.em_id=hrm_emp_contract_log.em_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -180,11 +180,12 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
              where hrm_branch.branch_slno IN (?)
              union all
              select hrm_emp_master.em_id,hrm_emp_master.em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,em_contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status,remark
             from hrm_emp_master
             left join hrm_employee on hrm_emp_master.em_id=hrm_employee.emp_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -195,6 +196,7 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
             where hrm_emp_master.em_status=0 and hrm_branch.branch_slno IN (?)`,
             [
                 data,
@@ -212,7 +214,7 @@ module.exports = {
         pool.query(
             `select hrm_emp_contract_log.em_id,old_emno as em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status,remark
             from hrm_emp_contract_log
             left join hrm_emp_master on hrm_emp_master.em_id=hrm_emp_contract_log.em_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -223,11 +225,12 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
              where em_branch IN (?) and em_department IN(?)
              union all
              select hrm_emp_master.em_id,hrm_emp_master.em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,em_contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status,remark
             from hrm_emp_master
             left join hrm_employee on hrm_emp_master.em_id=hrm_employee.emp_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -238,6 +241,7 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
             where hrm_emp_master.em_status=0 and em_branch IN (?) and em_department IN(?)`,
             [
                 data.em_branch,
@@ -257,7 +261,7 @@ module.exports = {
         pool.query(
             `select hrm_emp_contract_log.em_id,old_emno as em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(contract_end_date is not null,'Contract Closed',null)status,remark
             from hrm_emp_contract_log
             left join hrm_emp_master on hrm_emp_master.em_id=hrm_emp_contract_log.em_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -268,11 +272,12 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
              where  em_branch  IN (?) and em_department IN (?) and em_dept_section IN (?)
              union all
              select hrm_emp_master.em_id,hrm_emp_master.em_no,em_name,em_dob,if(em_gender=1,'Male','Female')em_gender,em_doj,em_mobile,em_email,branch_name,dept_name,
             sect_name,inst_emp_type,desg_name,doctype_desc,ecat_name,em_contract_end_date,em_adhar_no,
-            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status
+            em_retirement_date,addressPresent1,addressPresent2,hrm_pin2,if(hrm_emp_master.em_status=0,'Resigned',null)status,remark
             from hrm_emp_master
             left join hrm_employee on hrm_emp_master.em_id=hrm_employee.emp_id
             left join hrm_emp_personal on hrm_emp_personal.em_id=hrm_emp_master.em_id
@@ -283,6 +288,7 @@ module.exports = {
             left join designation on designation.desg_slno=hrm_emp_master.em_designation
             left join doctor_type on doctor_type.doctype_slno=hrm_emp_master.em_doc_type
             left join hrm_emp_category on hrm_emp_category.category_slno=hrm_emp_master.em_category
+            left join hrm_inactive_employee_details on hrm_emp_master.em_no=hrm_inactive_employee_details.em_no
             where hrm_emp_master.em_status=0 and em_branch IN (?) and em_department IN (?) and em_dept_section IN (?)`,
             [
                 data.em_branch,
