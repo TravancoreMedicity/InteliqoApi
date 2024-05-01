68f62da2cd53fa26a08f0106d205b8dfcfa08a0c
ManagerTMC <itmanager@tmc.ac.in>
2024-05-01 14:00:54 +0530

    error rectified in credit OFF get method

--- api/LeaveRequestApproval/LeaveRequestApproval.controller.js
+++ api/LeaveRequestApproval/LeaveRequestApproval.controller.js
@@ -2067,12 +2067,13 @@ module.exports = {
                     message: err
                 });
             }
-            if (!results) {
+            if (results?.length === 0) {
                 return res.status(200).json({
                     success: 2,
                     message: "Record Not Found"
                 });
             }
+
             return res.status(200).json({
                 success: 1,
                 data: results
