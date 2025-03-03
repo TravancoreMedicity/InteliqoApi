d781d9a9b8dadd26d8cf9cc0316f3a1be2d8e201
unknown <Administrator@ITSYS13.medicity.co.in>
2025-03-03 17:24:25 +0530

    Leave report for employee, manual reuest dsiable dutyplan

--- api/LeaveReport/LeaveReport.controller.js
+++ api/LeaveReport/LeaveReport.controller.js
@@ -1,4 +1,15 @@
-const { getleavereq, getHalfday, getNopunchReq, getCoffReq, getOneHour, getOnduty
+const {
+    getleavereq,
+    getHalfday,
+    getNopunchReq,
+    getCoffReq,
+    getOneHour,
+    getOnduty,
+    empMisspunchyearwise,
+    emOnehouryearwise,
+    empOndutyyearwise,
+    empHalfdayYearWise,
+    empLeaveYearWise
 } = require('../LeaveReport/LeaveReport.service');
 
 const logger = require('../../logger/logger')
@@ -117,7 +128,7 @@ module.exports = {
                     message: err
                 });
             }
-            else if (!results) {
+            else if (results.length === 0) {
                 return res.status(200).json({
                     success: 2,
                     message: "Record Not Found"
@@ -127,6 +138,127 @@ module.exports = {
                 success: 1,
                 data: results
             });
+        });
+    },
+    empMisspunchyearwise: (req, res) => {
+        const body = req.body;
+        empMisspunchyearwise(body, (err, results) => {
+            if (err) {
+                logger.errorLogger(err)
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            else if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "Record Not Found"
+                });
+            }
+            else {
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            }
+        });
+    },
+    emOnehouryearwise: (req, res) => {
+        const body = req.body;
+        emOnehouryearwise(body, (err, results) => {
+            if (err) {
+                logger.errorLogger(err)
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            else if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "Record Not Found"
+                });
+            }
+            else {
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            }
+        });
+    },
+    empOndutyyearwise: (req, res) => {
+        const body = req.body;
+        empOndutyyearwise(body, (err, results) => {
+
+            if (err) {
+                logger.errorLogger(err)
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            else if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "Record Not Found"
+                });
+            }
+            else {
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            }
+        });
+    },
+    empHalfdayYearWise: (req, res) => {
+        const body = req.body;
+        empHalfdayYearWise(body, (err, results) => {
+            if (err) {
+                logger.errorLogger(err)
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            else if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "Record Not Found"
+                });
+            }
+            else {
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            }
+        });
+    },
+    empLeaveYearWise: (req, res) => {
+        const body = req.body;
+        empLeaveYearWise(body, (err, results) => {
+            if (err) {
+                logger.errorLogger(err)
+                return res.status(200).json({
+                    success: 0,
+                    message: err
+                });
+            }
+            else if (results.length === 0) {
+                return res.status(200).json({
+                    success: 2,
+                    message: "Record Not Found"
+                });
+            }
+            else {
+                return res.status(200).json({
+                    success: 1,
+                    data: results
+                });
+            }
         });
     },
 }
--- api/LeaveReport/LeaveReport.router.js
+++ api/LeaveReport/LeaveReport.router.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const { checkToken } = require('../../auth/token_validation');
-const { getleavereq, getHalfday, getNopunchReq, getCoffReq, getOneHour, getOnduty
+const {
+    getleavereq,
+    getHalfday,
+    getNopunchReq,
+    getCoffReq,
+    getOneHour,
+    getOnduty,
+    empMisspunchyearwise,
+    emOnehouryearwise,
+    empOndutyyearwise,
+    empHalfdayYearWise,
+    empLeaveYearWise
 } = require('../LeaveReport/LeaveReport.controller');
 
 router.get("/leaverequest", checkToken, getleavereq)
@@ -10,5 +21,10 @@ router.get("/Coff", checkToken, getCoffReq)
 router.get("/OneHour", checkToken, getOneHour)
 router.get("/OnDuty", checkToken, getOnduty)
 
+router.post("/empMisspunchyearwise", checkToken, empMisspunchyearwise)
+router.post("/emOnehouryearwise", checkToken, emOnehouryearwise)
+router.post("/empOndutyyearwise", checkToken, empOndutyyearwise)
+router.post("/empHalfdayYearWise", checkToken, empHalfdayYearWise)
+router.post("/empLeaveYearWise", checkToken, empLeaveYearWise)
 
 module.exports = router;
