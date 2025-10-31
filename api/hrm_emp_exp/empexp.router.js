7700acfd7dc58665c166e9a6b3fa6aab535b58dc
unknown <Administrator@MEDICITY.CO.IN>
2025-10-31 12:23:20 +0530

    Delete' option for the Experience section

--- api/hrm_emp_exp/empexp.router.js
+++ api/hrm_emp_exp/empexp.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
-const { createExp, updateExp, getExpByID, getSelectAllExpByID, getDataByEmpno } = require('../hrm_emp_exp/empexp.controller');
+const { createExp, updateExp, getExpByID, getSelectAllExpByID, getDataByEmpno, DeleteByIdExp } = require('../hrm_emp_exp/empexp.controller');
 
 router.post("/", checkToken, createExp);
 router.patch("/", checkToken, updateExp);
@@ -8,5 +8,6 @@ router.get("/select/:id", checkToken, getSelectAllExpByID);
 router.get("/:id", checkToken, getExpByID);
 
 router.get("/dataByEmpno/:id", checkToken, getDataByEmpno)
+router.delete("/:id", checkToken, DeleteByIdExp);
 
 module.exports = router;
